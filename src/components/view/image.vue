e6223df841f4b80db53aeaf0a816291807788e23
Dafrok <o.o@mug.dog>
2016-08-19 18:22:02 +0800

    Image

---
+++ src/components/view/image.vue
@@ -0,0 +1,27 @@
+<template lang="jade">
+div.image-wrapper
+  img(:src="src", style="{display: 'none'}", @load="onImageLoad")
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      imageLoaded: false,
+      imageWidth: null,
+      imageHeight: null
+    }
+  },
+  methods: {
+    onImageLoad (e) {
+      const image = e.target
+      this.imageWidth = image.width
+      this.imageHeight = image.height
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
